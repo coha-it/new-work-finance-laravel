ee10ff65d0ee973eb04e8418f6baa8070e7b8809
abachschmid <>
2020-02-07 18:23:36 +0100

    Registerbutton

--- resources/views/components/registerbutton.blade.php
+++ resources/views/components/registerbutton.blade.php
@@ -1,7 +1,7 @@
 <div class="flying-wrapper coha--btn">
     <div class="border-btn-box pointer-large dark-bg-1">
         <div class="border-btn-inner">
-            <a href="{{ route('contact') }}" class="border-btn" data-text="Anmelden">Anmelden</a>
+            <a href="{{ route('register') }}" class="border-btn" data-text="Anmelden">Anmelden</a>
         </div>
     </div>
 </div>
