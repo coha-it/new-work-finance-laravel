609ef670810556f06f0489e0841ee8f37f02f81a
Alexander Bachschmid <alexander.bachschmid@prochannel.eu>
2020-02-07 15:30:04 +0100

    Styling and RSFK

--- resources/views/components/contactbutton.blade.php
+++ resources/views/components/contactbutton.blade.php
@@ -2,7 +2,7 @@
 <div class="flying-wrapper coha--btn">
     <div class="border-btn-box pointer-large dark-bg-1">
         <div class="border-btn-inner">
-            <a href="#" class="border-btn" data-text="Anmelden">Anmelden</a>
+            <a href="{{ route('contact') }}" class="border-btn" data-text="Anmelden">Anmelden</a>
         </div>
     </div>
 </div>
--- resources/views/components/home.blade.php
+++ resources/views/components/home.blade.php
@@ -74,9 +74,9 @@
               <!-- column start -->
               <div class="twelve-columns">
 								<div class="content-right-margin-20 content-left-margin-20">
-									<p data-animation-child="" class="coha--text-split-2 p-style-medium text-color-5 fade-anim-box tr-delay02 animated fade-anim" data-animation="fade-anim">Unser New Work Finance Programm zeigt Ihnen, wie Sie fernab von herkömmlichen Schulungsansätzen effektiv und nachhaltig, verschiedenste Herausforderungen der Finanzbranche lösen können, um Ihr Unternehmen von Innen heraus zu stärken und in eine erfolgreiche Zukunft zu führen.
-                    Unsere einzigartige Methodik, basierend auf wissenschaftlich fundierten Erkenntnissen, sichert die Freisetzung bisher unentdeckter Selbstentfaltungskräfte Ihrer MitarbeiterInnen und Mitarbeiter. Eigenverantwortung, Kreativität und Wertschätzung sind Schlüsselqualifikationen, die unabdingbar für ein modernes Unternehmen sind. Wir schulen daher nicht auf der Informationsebene, sondern bringen die Teilnehmerinnen und Teilnehmer ins Erfahren. Nur wer die Veränderungen seiner eigenen Wirksamkeit erlebt, kann auch andere dazu inspirieren.
-                    In den neunmonatigen Programmen, setzen wir auf die positiven Effekte des blended-Learnings, d.h. eine Kombination aus Präsenz-Workshops und einer umfangreichen eLearning-Plattform. Sie werden somit zielgerichtet und praxisnah ausgebildet, um Vorreiter der Branche zu werden.</p>
+									<p data-animation-child="" class="coha--text-split-2 p-style-medium text-color-5 fade-anim-box tr-delay02 animated fade-anim" data-animation="fade-anim">Unser New Work Finance Ausbildungsprogramm zeigt Ihnen, wie Sie fernab von herkömmlichen Schulungsansätzen effektiv und nachhaltig, verschiedenste Herausforderungen der Finanzbranche lösen können, um Ihr Unternehmen von Innen heraus zu stärken und in eine erfolgreiche Zukunft zu führen.
+                    Unsere einzigartige Methodik, basierend auf wissenschaftlich fundierten Erkenntnissen, sichert die Freisetzung bisher unentdeckter Selbstentfaltungskräfte Ihrer MitarbeiterInnen und Mitarbeiter. Eigenverantwortung, Kreativität und Wertschätzung sind Schlüsselqualifikationen, die unabdingbar für ein modernes Unternehmen sind. Wir schulen daher nicht nur auf der Informationsebene, sondern bringen die Teilnehmerinnen und Teilnehmer ins Erfahren. Nur wer die Veränderungen seiner eigenen Wirksamkeit erlebt, kann auch andere dazu inspirieren.
+                    In dem neunmonatigen Programm, setzen wir auf die positiven Effekte des Blended-Learnings, d.h. eine Kombination aus Präsenz-Workshops und einer umfangreichen eLearning-Plattform. Sie werden somit zielgerichtet und praxisnah ausgebildet, um Vorreiter der Branche zu werden.</p>
 								</div>
 							</div>
 
@@ -114,7 +114,7 @@
 										<h3 class="title-style text-color-1">
 											<span data-animation-child class="overlay-anim-box2 overlay-dark-bg-2 tr-delay01" data-animation="overlay-anim2">Aufbruchsstimmung in der </span><br>
 											<span data-animation-child class="overlay-anim-box2 overlay-dark-bg-2 tr-delay01" data-animation="overlay-anim2">Digitalisierung!</span><br>
-											<span data-animation-child class="overlay-anim-box2 overlay-dark-bg-2 tr-delay03 small-title-oswald" data-animation="overlay-anim2">Wie man die digitale Transformation optimal ausschöpft</span><br>
+											<span data-animation-child class="overlay-anim-box2 overlay-dark-bg-2 tr-delay03 small-title-oswald" data-animation="overlay-anim2">Wie Sie die digitale Transformation optimal ausschöpfen</span><br>
                     </h3>
                     <div class="coha--cut-text-wrapper">
   										<div data-animation-child class="p-style-small text-color-1 fade-anim-box tr-delay04" data-animation="fade-anim">
@@ -122,26 +122,26 @@
                           Die Digitalisierung im Bankensektor schreitet voran und bringt massive Änderungen mit sich. Menschliche Tätigkeiten werden zunehmend durch technische Lösungen ersetzt und das Anforderungsprofil der Mitarbeiter steht zunehmend im Wandel. Trotz den enormen Investitionen der Banken in digitale Technologien bleiben die erhofften Erträge und die damit verbundenen Einsparungen allerdings bisher größtenteils aus.<br>
                         </p>
                        <p>
-                         <b>In ihrer New Work Finance Ausbildung lernen Sie:</b>
+                         <b>In Ihrer New Work Finance Ausbildung lernen Sie:</b>
                        </p>
-                       <ul>
+                       <ul class="bullets">
                          <li>
-                           wie und weshalb Vorreiter der Branche in der digitalen Transformation auf den Erfolgsfaktor Mensch setzen und genau damit die erhofften Fortschritte und Effizienzgewinne erzielen.
+                           wie und weshalb Vorreiter der Branche in der digitalen Transformation auf den Erfolgsfaktor Mensch setzen und genau damit die erhofften Fortschritte und Effizienzgewinne erzielen
                          </li>
                          <li>
-                           welche Chancen Ihnen die Digitalisierung persönlich und als Unternehmen eröffnet und wie Sie den Herausforderungen wirksam begegnen.
+                           welche Chancen Ihnen die Digitalisierung persönlich und als Unternehmen eröffnet und wie Sie den Herausforderungen wirksam begegnen
                          </li>
                          <li>
-                           wie Sie Mitarbeiter auf dem Weg durch die Digitalisierung kurz- und langfristig mobilisieren und nicht verlieren.
+                           wie Sie Mitarbeiter auf dem Weg durch die Digitalisierung kurz- und langfristig mobilisieren und nicht verlieren
                          </li>
                          <li>
-                           wie Sie als Unternehmen die Digitalisierung für eine höhere Arbeitgeberattraktivität nutzen können.
+                           wie Sie als Unternehmen die Digitalisierung für eine höhere Arbeitgeberattraktivität nutzen können
                          </li>
                          <li>
-                           dass Sie sich jetzt mit Persönlichkeits-entwicklung auf die Digitalisierung vorbereiten sollten.
+                           wie Sie sich jetzt mit Persönlichkeits-entwicklung auf die Digitalisierung vorbereiten sollten
                          </li>
                          <li>
-                           warum die Zukunft – aufgrund der Digitalisierung - der Mensch ist.
+                           warum die Zukunft – aufgrund der Digitalisierung - der Mensch ist
                          </li>
                         </ul>
                       </div>
@@ -187,16 +187,14 @@
                       <div data-animation-child class="p-style-small text-color-1 fade-anim-box tr-delay04" data-animation="fade-anim">
                         <p>Faktoren wie der Personalabbau, die Finanzkrise, die Schließung von Filialen oder die Kürzung der Ausbildungsquote haben das einstmals nahezu unanfechtbare Arbeitgeberimage der Banken nachhaltig beschädigt.</p>
                         <p>Die früheren Attribute „sicherer Arbeitsplatz“, „solides Berufsbild“ scheinen heute nicht mehr hinreichend verlässlich zu sein. Die Arbeitgeberattraktivität von Banken und Sparkassen ist heute nur noch mittelmäßig. Und das ist zu wenig, um auf einem Arbeitsmarkt mit wachsenden strukturellen Angebotsschwächen Erfolg zu haben.</p>
-                        <strong>In ihrer New Work Finance Ausbildung lernen Sie:</strong>
-                        <p>
-                            <ol>
-                              <li>-	warum Stellenanzeigen und Messen das Problem nicht lösen werden.</li>
-                              <li>-	dass die Mitarbeitergewinnung nicht das eigentliche Problem ist. </li>
-                              <li>-	was bei Bewerbern noch viel wichtiger ist, als die fachliche Qualifikation.</li>
-                              <li>-	dass man bei der Gewinnung von Mitarbeitern nicht sparen muss, um wirtschaftlich erfolgreich zu sein. </li>
-                              <li>-	wie ihre Bank trotz Krise zum Fachkräftemagneten wird und zu alter Arbeitgeberattraktivität zurückfindet</li>
-                            </ol>
-                        </p>
+                        <strong>In Ihrer New Work Finance Ausbildung lernen Sie:</strong>
+                        <ul class="bullets">
+                          <li>warum Stellenanzeigen und Messen das Problem nicht lösen werden</li>
+                          <li>dass die Mitarbeitergewinnung nicht das eigentliche Problem ist</li>
+                          <li>was bei Bewerbern noch viel wichtiger ist, als die fachliche Qualifikation</li>
+                          <li>dass Sie bei der Gewinnung von Mitarbeitern nicht sparen dürfen und müssen, um wirtschaftlich erfolgreich zu sein</li>
+                          <li>wie ihre Bank trotz Krise zum Fachkräftemagneten wird und zu alter Arbeitgeberattraktivität zurückfindet</li>
+                        </ul>
                       </div>
                       <div class="after"></div>
                     </div>
@@ -223,10 +221,10 @@
 								<!-- column start -->
 								<div data-animation-container class="six-columns top-padding-60 bottom-padding-60 ">
 									<div class="content-right-margin-40">
-										<span class="small-title-oswald gold-color overlay-anim-box2" data-animation="overlay-anim2">Agilitätskultur & neue Geschäftsmodelle</span>
+										{{-- <span class="small-title-oswald gold-color overlay-anim-box2" data-animation="overlay-anim2">Agilitätskultur & neue Geschäftsmodelle</span> --}}
 										<h3 class="title-style text-color-1">
 											<span data-animation-child class="overlay-anim-box2 overlay-dark-bg-2 tr-delay01" data-animation="overlay-anim2">Corporate Identity</span><br>
-											<span data-animation-child class="overlay-anim-box2 overlay-dark-bg-2 tr-delay03 small-title-oswald" data-animation="overlay-anim2">trotz Fusionen und Zentralisierung!</span><br>
+											<span data-animation-child class="overlay-anim-box2 overlay-dark-bg-2 tr-delay03 small-title-oswald" data-animation="overlay-anim2">Agilitätskultur & neue Geschäftsmodelle</span><br>
                     </h3>
 
                     <div class="coha--cut-text-wrapper">
@@ -234,17 +232,15 @@
                         <p>Deutsche Banken sind hochgradig zinsabhängig. Im aktuellen Niedrigzinsumfeld ist das ein Problem, denn die Situation wird sich in absehbarerer Zeit durch die Niedrigzinspolitik der EZB nicht ändern.</p>
                         <p>Zur schlechten Ertragslage gesellen sich enorme Kosten, verursacht durch Investitionen in die digitale Transformation und Restrukturierungskosten des Personalabbaus.</p>
                         <p>Aufgrund der daraus resultierenden Einsparungen kommt es immer häufiger zu Fusionen und Zusammenschlüssen, welche Banken vor große Kultur und Management Herausforderungen stellen.</p>
-                        <p>In ihrer New Work Finance Ausbildung lernen Sie:</p>
-                        <p>
-                            <ol>
-                                <li>Wie Fusionen und Zusammenschlüsse zum Profit werden können</li>
-                                <li>Was es dazu braucht neue Geschäftsbereiche zu erschließen und sich somit weniger von Zinserträgen abhängig zu machen</li>
-                                <li>Weshalb agile Mitarbeiter für den Kulturwandel eines Zusammenschlusses unabdingbar sind</li>
-                                <li>Wie Mitarbeiter agiler, begeisterter und kreativer werden und das Unternehmen von innen heraus stärken</li>
-                                <li>Welchen Beitrag Führungskräfte und Mitarbeiter zu einer neuen Agilitätskultur beitragen können</li>
-                                <li>Warum ein intaktes Leitbild dabei eine entscheidende Rolle spielt</li>
-                            </ol>
-                        </p>
+                        <p>In Ihrer New Work Finance Ausbildung lernen Sie:</p>
+                            <ul class="bullets">
+                                <li>wie Fusionen und Zusammenschlüsse zum Profit werden können, wenn die Kulturthemen beachtet werden</li>
+                                <li>was Sie dazu brauchen, um neue Geschäftsbereiche zu erschließen und sich somit weniger von Zinserträgen abhängig zu machen</li>
+                                <li>weshalb agile Mitarbeiter für den Kulturwandel eines Zusammenschlusses unabdingbar sind</li>
+                                <li>wie Mitarbeiter agiler, begeisterter und kreativer werden und das Unternehmen von innen heraus stärken</li>
+                                <li>welchen Beitrag Führungskräfte und Mitarbeiter zu einer neuen Agilitätskultur beitragen können</li>
+                                <li>warum ein intaktes Leitbild dabei eine entscheidende Rolle spielt</li>
+                            </ul>
 
                       </div>
                       <div class="after"></div>
@@ -289,95 +285,43 @@
 						<li>
 							<a href="#" class="pointer-large d-block">
 								<div class="brand-box single">
+									<img src="{{ asset('assets/images/logos/vr-bank-wuerzburg.svg') }}" alt="Brand" class="opac-img">
+								</div>
+							</a>
+                        </li>
+						<li>
+							<a href="#" class="pointer-large d-block">
+								<div class="brand-box single">
 									<img src="{{ asset('assets/images/logos/kasseler-sparkasse.png') }}" alt="Brand" class="opac-img">
 								</div>
 							</a>
-						</li>
+                        </li>
 						<li>
 							<a href="#" class="pointer-large d-block">
-								<div class="brand-box">
-									<img src="assets/images/clients/6b9fcb90-c595-432a-a97c-fcd318b74c04.png" alt="Brand" class="hover-opac-img">
-									<img src="assets/images/clients/699301b6-83ff-47f8-ad46-a6b871e79293.png" alt="Brand" class="opac-img">
+								<div class="brand-box single">
+									<img src="{{ asset('assets/images/logos/itdz-berlin.svg') }}" alt="Brand" class="opac-img">
 								</div>
 							</a>
-						</li>
+                        </li>
 						<li>
 							<a href="#" class="pointer-large d-block">
-								<div class="brand-box">
-									<img src="assets/images/clients/7d1fe64f-fbd7-4e5f-896d-6d79c91f29bf.png" alt="Brand" class="hover-opac-img">
-									<img src="assets/images/clients/858e2e28-faae-441c-bd9c-5c490c72b562.png" alt="Brand" class="opac-img">
+								<div class="brand-box single">
+									<img src="{{ asset('assets/images/logos/telekom.svg') }}" alt="Brand" class="opac-img">
 								</div>
 							</a>
-						</li>
+                        </li>
 						<li>
 							<a href="#" class="pointer-large d-block">
-								<div class="brand-box">
-									<img src="assets/images/clients/8b587214-bdcb-4d74-bb61-2480d1d15aa7.png" alt="Brand" class="hover-opac-img">
-									<img src="assets/images/clients/4b983c9a-759d-4921-ba60-dbbcaa702ee5.png" alt="Brand" class="opac-img">
-								</div>
-							</a>
-						</li>
-						<li>
-							<a href="#" class="pointer-large d-block">
-								<div class="brand-box">
-									<img src="assets/images/clients/8f65e211-cd41-46dc-8c1d-ba228ac36eb9.png" alt="Brand" class="hover-opac-img">
-									<img src="assets/images/clients/9c30d6d6-6af9-4fb1-bed3-e5ea4df4c281.png" alt="Brand" class="opac-img">
-								</div>
-							</a>
-						</li>
-						<li>
-							<a href="#" class="pointer-large d-block">
-								<div class="brand-box">
-									<img src="assets/images/clients/4731da4b-b2fb-4c10-aee7-c89484a5d473.png" alt="Brand" class="hover-opac-img">
-									<img src="assets/images/clients/69cb9e61-d0b8-4420-b8bd-454c9b9e1dc1.png" alt="Brand" class="opac-img">
-								</div>
-							</a>
-						</li>
-						<li>
-							<a href="#" class="pointer-large d-block">
-								<div class="brand-box">
-									<img src="assets/images/clients/492492b2-b7d9-4648-a164-f530ecd6321a.png" alt="Brand" class="hover-opac-img">
-									<img src="assets/images/clients/9a6c570f-d03a-4806-9a30-dddc648707d9.png" alt="Brand" class="opac-img">
-								</div>
-							</a>
-						</li>
-						<li>
-							<a href="#" class="pointer-large d-block">
-								<div class="brand-box">
-									<img src="assets/images/clients/b908b85c-f837-4dba-80cf-d961d6fcad29.png" alt="Brand" class="hover-opac-img">
-									<img src="assets/images/clients/6e63c653-6997-42c6-b788-28e9fb6d15b7.png" alt="Brand" class="opac-img">
-								</div>
-							</a>
-						</li>
-						<li>
-							<a href="#" class="pointer-large d-block">
-								<div class="brand-box">
-									<img src="assets/images/clients/d5f831cf-3fb0-490b-aadc-3591c8b64156.png" alt="Brand" class="hover-opac-img">
-									<img src="assets/images/clients/2ed0436b-e3ea-48aa-85ca-755de0719c9a.png" alt="Brand" class="opac-img">
-								</div>
-							</a>
-						</li>
-						<li>
-							<a href="#" class="pointer-large d-block">
-								<div class="brand-box">
-									<img src="assets/images/clients/d51e13e6-1320-4227-8bc8-c36aaa82dffd.png" alt="Brand" class="hover-opac-img">
-									<img src="assets/images/clients/aed14647-8ecf-4fef-8274-737d79a7ede1.png" alt="Brand" class="opac-img">
-								</div>
-							</a>
-						</li>
-						<li>
-							<a href="#" class="pointer-large d-block">
-								<div class="brand-box">
-									<img src="{{ asset('assets/images/clients/d383cf8d-abee-4290-810d-a965f6c0fa6c.png') }}" alt="Brand" class="hover-opac-img">
-									<img src="{{ asset('assets/images/clients/7f7b6ede-3a3e-4e17-af25-f881c0121d63.png') }}" alt="Brand" class="opac-img">
+								<div class="brand-box single">
+									<img src="{{ asset('assets/images/logos/jobcenter-muenchen.png') }}" alt="Brand" class="opac-img">
 								</div>
 							</a>
 						</li>
 						<li class="empty-spot-box">
 							<a data-animation-container href="#" class="pointer-large p-style-bold-up empty-spot d-block">
-								<span data-animation-child class="title-fill" data-animation="title-fill-anim" data-text="This spot">This spot</span>
-								<span data-animation-child class="title-fill tr-delay01" data-animation="title-fill-anim" data-text="Awaits">Awaits</span>
-								<span data-animation-child class="title-fill tr-delay02" data-animation="title-fill-anim" data-text="You">You</span>
+								<span data-animation-child class="title-fill" data-animation="title-fill-anim" data-text="Dieser Platz">Dieser Platz</span>
+								<span data-animation-child class="title-fill tr-delay01" data-animation="title-fill-anim" data-text="wartet">wartet</span>
+								<span data-animation-child class="title-fill tr-delay02" data-animation="title-fill-anim" data-text="auf Sie">auf Sie</span>
 							</a>
 						</li>
 					</ul><!-- client-list end -->
