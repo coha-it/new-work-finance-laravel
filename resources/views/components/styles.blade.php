40468bf9f16887f076436365357312435b489455
abachschmid <>
2020-03-25 02:43:56 +0100

    font swap

--- resources/views/components/styles.blade.php
+++ resources/views/components/styles.blade.php
@@ -1,4 +1,4 @@
-<link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,500,600,700%7COswald:300,400,500,600,700" rel="stylesheet" type="text/css">
+<link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,500,600,700%7COswald:300,400,500,600,700&display=swap" rel="stylesheet" type="text/css">
 
 @if(App::environment('production'))
 	<link href="{{ mix('assets/css/combined.min.css') }}" rel="stylesheet" type="text/css">
